fe5f17e9f5363414f7f06c4360c1175566f008b0
Rahul Joshi <rahul.joshi7181@gmail.com>
2024-07-20 12:01:08 +0530

    message

--- models/db-common.js
+++ models/db-common.js
@@ -1,6 +1,6 @@
 const executeQuery = async (query, params, db) => {
     try {
-        console.log('HELLO')
+        console.log('HELLO', 'HH')
         const [results] = await db.execute(query, params)
         return results
     } catch (err) {
